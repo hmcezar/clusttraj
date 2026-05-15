e108f3e21909c1c16374fe8eb5ae590b0b5f5430
Henrique Musseli Cezar <henrique.musseli@gmail.com>
2026-05-15 14:54:01 +0200

    Apply black formatting to address PR review

--- test/test_classify.py
+++ test/test_classify.py
@@ -1,13 +1,30 @@
 import pytest
-from clusttraj.classify import classify_structures, classify_structures_nclusters, classify_structures_silhouette
+from clusttraj.classify import (
+    classify_structures,
+    classify_structures_nclusters,
+    classify_structures_silhouette,
+)
 
 
 def test_classify_structures_silhouette(clust_opt):
     import numpy as np
-    # This distmat reliably yields a single optimal threshold, causing an IndexError
-    distmat = np.array([0.8914448312585805, 1.016370717302439, 1.0770540172126273, 0.5262279763713829, 0.9056642470478309, 0.7088570464110961, 0.49162431604355217, 0.8800215058217346, 0.6122727896102328, 0.7668009091886758])
-    classify_structures_silhouette(clust_opt, distmat, dstep=0.1)
 
+    # This distmat reliably yields a single optimal threshold, causing an IndexError
+    distmat = np.array(
+        [
+            0.8914448312585805,
+            1.016370717302439,
+            1.0770540172126273,
+            0.5262279763713829,
+            0.9056642470478309,
+            0.7088570464110961,
+            0.49162431604355217,
+            0.8800215058217346,
+            0.6122727896102328,
+            0.7668009091886758,
+        ]
+    )
+    classify_structures_silhouette(clust_opt, distmat, dstep=0.1)
 
 
 def test_classify_structures(clust_opt, test_distmat, Z_matrix, clusters_seq):
